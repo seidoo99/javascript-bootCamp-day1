7af918982e9934512d02a0e441034571df5c5c3f
Seid Zerihun <seidzerihun@Seids-MacBook-Pro.local>
2020-01-23 20:15:17 -0500

    second-day

---
+++ scrip.js
@@ -0,0 +1,11 @@
+var two = 2;
+var onefourth = "1/4";
+
+function marinarasauce(cookName) {
+    console.log('Hey,' + cookName + ' cook Heat a medium - large saucepan over medium heat');
+    console.log('Hey,' + cookName + ' Add ' + two + 'tspns of oil and 5 carlic cloves, cook until golden, about 2 minutes');
+    console.log('hey' + cookName + ' Add ' + onefourth + 'cup water, 2 cans of crushed tomates, and salt and season with black pepper to taste.');
+    console.log('hey' + cookName + ' Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes');
+    console.log('hey' + cookName + ' Stir in' + onefourth + 'cup roughly chopped fresh basil, salt and pepper as needed.');
+}
+marinarasauce('seid');
